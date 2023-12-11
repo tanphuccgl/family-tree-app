099033ec8030132f306aa8b7ab1eafb2572bc058
Levi <phamnguyentanphucgl@gmail.com>
2023-12-11 14:09:56 +0700

    add responsive for drawer

--- lib/src/features/dashboard/pages/dashboard_page.dart
+++ lib/src/features/dashboard/pages/dashboard_page.dart
@@ -1,17 +1,45 @@
 import 'package:familytree/src/features/dashboard/cubit/dashboard_bloc.dart';
 import 'package:familytree/src/features/dashboard/widgets/drawer/drawer_dashboard.dart';
+import 'package:familytree/src/features/dashboard/widgets/drawer/expanded/drawer_expanded.dart';
+import 'package:familytree/src/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return BlocProvider(
-      create: (context) => DashboardBloc(),
+      create: (context) => DashboardBloc(context),
       child: BlocBuilder<DashboardBloc, DashboardState>(
         builder: (context, state) {
+          final isMobile = ResponsiveBreakpoints.of(context).isMobile;
+          if (isMobile || size.width < 1120) {
+            return Scaffold(
+              key: context.read<DashboardBloc>().scaffoldKeyDrawer,
+              drawer: Drawer(
+                child: DrawerExpanded(isPopUp: true),
+              ),
+              appBar: AppBar(
+                elevation: 0,
+                backgroundColor: XColors.primary2,
+                leading: IconButton(
+                  icon: Icon(Icons.menu),
+                  onPressed: () => context.read<DashboardBloc>().openDrawer(),
+                ),
+              ),
+              body: PageView(
+                controller: context.read<DashboardBloc>().pageController,
+                physics: const NeverScrollableScrollPhysics(),
+                children: state.pages,
+              ),
+            );
+          }
+
           return Scaffold(
             body: Row(
               mainAxisSize: MainAxisSize.max,
@@ -19,7 +47,6 @@ class DashboardPage extends StatelessWidget {
               children: <Widget>[
                 DrawerDashboad(),
                 Expanded(
-                  flex: state.isExpandedDrawer ? 80 : 95,
                   child: PageView(
                     controller: context.read<DashboardBloc>().pageController,
                     physics: const NeverScrollableScrollPhysics(),
--- lib/src/features/dashboard/widgets/drawer/expanded/drawer_expanded.dart
+++ lib/src/features/dashboard/widgets/drawer/expanded/drawer_expanded.dart
@@ -6,42 +6,45 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DrawerExpanded extends StatelessWidget {
-  const DrawerExpanded({super.key});
+  final bool isPopUp;
+  const DrawerExpanded({super.key, this.isPopUp = false});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return BlocBuilder<DashboardBloc, DashboardState>(
       builder: (context, state) {
-        return Expanded(
-          flex: 20,
-          child: Container(
-            color: XColors.primary1,
-            padding: EdgeInsets.fromLTRB(0, 30, 0, 10),
-            child: Column(
-              children: [
-                Expanded(
-                  child: ListView(
-                      shrinkWrap: true,
-                      children: ItemDrawerEnum.values
-                          .map((e) => ItemDrawer(data: e))
-                          .toList()),
-                ),
-                divider(),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 10),
-                      child: IconButton(
-                        onPressed: () =>
-                            context.read<DashboardBloc>().onDrawerButton(),
-                        icon: Icon(
-                          Icons.arrow_back_ios,
-                          color: XColors.primary5,
-                        ),
-                      )),
-                )
-              ],
-            ),
+        return Container(
+          width: 300,
+          height: size.height,
+          color: XColors.primary1,
+          padding: EdgeInsets.fromLTRB(0, 30, 0, 10),
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView(
+                    shrinkWrap: true,
+                    children: ItemDrawerEnum.values
+                        .map((e) => ItemDrawer(data: e))
+                        .toList()),
+              ),
+              divider(),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: IconButton(
+                      onPressed: isPopUp
+                          ? () => context.read<DashboardBloc>().closeDrawer()
+                          : () =>
+                              context.read<DashboardBloc>().onDrawerButton(),
+                      icon: Icon(
+                        Icons.arrow_back_ios,
+                        color: XColors.primary5,
+                      ),
+                    )),
+              )
+            ],
           ),
         );
       },
