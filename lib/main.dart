069eb3ca5c30468072c82e8664f35e8d68169ea3
Levi <phamnguyentanphucgl@gmail.com>
2023-12-20 11:37:41 +0700

    Configuring the URL strategy

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:familytree/src/locator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'src/app.dart';
 
 Future main() async {
+  usePathUrlStrategy();
   await ConfigApp.initializeApp();
   runApp(
     const MyApp(),
